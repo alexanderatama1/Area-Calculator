b33a71b780684094a71225fb673eee5ca758486d
Anthony Alexander Atama <100134398+alexanderatama1@users.noreply.github.com>
2023-10-08 17:16:04 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Declare the variables
+int length, width;
+
+// Prompt for length
+Console.Write("Please enter length: ");
+length = int.Parse (Console.ReadLine());
+
+// Prompt for width
+Console.Write("Please enter width: ");
+width = int.Parse (Console.ReadLine());
+
+//Output area to the Console
+Console.WriteLine("A rectangle with dimensions of " + length + "x" + width + " has an area of " + length*width + ".");
